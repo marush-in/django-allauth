4eab7ec339ce132908a49b10bb9cbae2279dac04
marush-in <shin.marumoto@gmail.com>
2019-02-16 09:18:11 +0900

    Developed basic django-allauth settings.

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 ]
 
 MIDDLEWARE = [
@@ -106,3 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# django-allauth
+SITE_ID = 1
+LOGIN_REDIRECT_URL = 'hone'
+ACCOUNT_LOGOUT_REDIRECT_URL = '/account/login'
